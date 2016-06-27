7745cda9c99d3c5aa569242d313e309545a3c3a5
hockey2249 <kgibbons14@gmail.com>
2016-06-27 09:03:29 -0600

    OOP Project

--- oop/js_oop.js
+++ oop/js_oop.js
@@ -1,124 +1,77 @@
-var normalGame = new Game('X', 'O');
-var display = 'X';
-var boxColor = "red";
+function Player (player, boxColor) {
+    this.player = player;
+    this.boxColor = boxColor;        
+}
+function Game(){
+    this.theBoxes = document.getElementsByClassName("box"); 
+    this.count = 0;
+    this.player1 = new Player("x", "green");
+    this.player2 = new Player("o", "blue");
 
-function Game (player1Name, player2Name) {
-  this.board = new Board();
-  this.player1 = new Player(player1Name);
-  this.player2 = new Player(player2Name);
-  this.currentUser = this.player1;
-  
-  this.start = function() {
-	message(this.currentUser + ", please make your move first.");
-  };
-
-  this.message = function() {
-    document.getElementById("messages").textContent = mess;
-  };
-  this.switchTurn = function() {
-  	 if(checkForWinner(display)){
-		message("Congratulations, " + display + "! You win!");
-		winner = display;
-		alert(display + ", you won! Please don't click any more spaces.");
-	}else if (display == 'X'){
-		display = 'O';
-		boxColor = "blue";
-		message(display + ", please make your move.");
-	} else {
-		display = 'X';
-		message(display + ", please make your move.");
-		boxColor = "red";
-	}
-	message.textContent = display + " goes!";
+Game.prototype.init = function(){
+for (var i = 0; i < this.theBoxes.length; i++){
+    this.theBoxes[i].addEventListener("click", this.turn.bind(this));
+    }
+};
+Game.prototype.turn = function(){
+    console.log(this.checkForWinner());
+    if (!this.checkForWinner()){
+        this.count += 1;
+    if(event.target.textContent.length === 0){
+        if (this.count%2 !== 0 ){
+            event.target.textContent= this.player1.player;
+               event.target.style.backgroundColor = this.player1.boxColor;
+        } else {
+            event.target.textContent = this.player2.player;
+            event.target.style.backgroundColor = this.player2.boxColor;
+        }    
+    }
+    if (this.checkForWinner()){
+        var messageEl = document.querySelector('#messages');
+        if(messageEl){
+            messageEl.textContent = "You are the Winner!!!";
+        }
+    } 
+    }
 };
 }
-
-Game.prototype.start = function() {
-	var display = 'X';	
-	message(display + ", please make your move first.");
-  };
-
-Game.prototype.message = function(mess) {
-    document.getElementById("messages").textContent = mess;
+Game.prototype.getBox = function(number) {
+    return document.getElementById("s" + number).textContent;
 };
-
-Game.prototype.switchTurn = function() {
-  	if(checkForWinner(display)){
-		message("Congratulations, " + display + "! You win!");
-		winner = display;
-		alert(display + ", you won! Please don't click any more spaces.");
-	}else if (display == 'X'){
-		display = 'O';
-		boxColor = "blue";
-		message(display + ", please make your move.");
-	} else {
-		display = 'X';
-		message(display + ", please make your move.");
-		boxColor = "red";
-	}
-	message.textContent = display + " goes!";
+Game.prototype.checkRow = function(a, b, c) {
+    if(this.getBox(a) && this.getBox(b) && this.getBox(c) && this.getBox(a) === this.getBox(b)&& this.getBox(b)=== this.getBox(c)) {
+        return true;
+    }
+    return false;
 };
-
-Game.prototype.makeMove = function() {
-	if (box.textContent == ''){
-		box.textContent = display;
-		box.style.backgroundColor = boxColor;
-		switchTurn();
-	}else {
-		message('Please choose another square, this square is already taken!');
-	}
+Game.prototype.checkForWinner = function() {
+    if( this.checkRow(1,2,3) || 
+        this.checkRow(4,5,6) || 
+        this.checkRow(7,8,9) ||
+        this.checkRow(1,4,7) ||
+        this.checkRow(2,5,8) ||
+        this.checkRow(3,6,9) ||
+        this.checkRow(1,5,9) ||
+        this.checkRow(3,5,7)) {
+        
+        return true;
+    }
+    return false;
 };
+function init(){
+    console.log("Main init function");
+    var myGame = new Game();
+    myGame.init();
 
-function Player(name) {
-  this.name = name;
+    document.getElementById("playAgain").addEventListener("click", clearBoard);
+    function clearBoard(){
+        myGame.theBoxes = document.getElementsByClassName("box");
+        for(var i = 0; i < myGame.theBoxes.length; i++)  {
+           myGame.theBoxes[i].textContent ="";
+           myGame.theBoxes[i].style.backgroundColor = "white";
+    }
+    document.querySelector('#messages').textContent = "";
+    }
 }
-function Board () {
-  this.board = {
-    s1: document.querySelector('#s1'),
-    s2: document.querySelector('#s2'),
-    s3: document.querySelector('#s3'),
-    s4: document.querySelector('#s4'),
-    s5: document.querySelector('#s5'),
-    s6: document.querySelector('#s6'),
-    s7: document.querySelector('#s7'),
-    s8: document.querySelector('#s8'),
-    s9: document.querySelector('#s9'),
-  };
-}
+window.onload = init;
 
-Board.prototype.checkForWinner = function(allow) {
-	var result = false;
-	
-	if( checkRow(1,2,3, allow) || 
-		checkRow(4,5,6, allow) || 
-		checkRow(7,8,9, allow) ||
-		checkRow(1,4,7, allow) ||
-		checkRow(2,5,8, allow) ||
-		checkRow(3,6,9, allow) ||
-		checkRow(1,5,9, allow) ||
-		checkRow(3,5,7, allow)) {
-		
-		result = true;
-	}
-	return result;
-};
-
-Board.prototype.checkRow = function(a, b, c, allow) {
-	var result = false;
-	if(getBox(a) == allow && getBox(b) == allow && getBox(c) == allow) {
-		result = true;
-	}
-	return result;
-};
-
-Board.prototype.getBox = function(number) {
-	return document.getElementById("s" + number).textContent;
-};
-Board.prototype.reset = function() {
-	var gameBoxes = document.getElementsByClassName("box");
-		for (var i = 0; i < gameBoxes.length; i++){
-			gameBoxes[i].style.backgroundColor='purple';
-			gameBoxes[i].innerHTML= '';
-	}
-		return start();		
-};
